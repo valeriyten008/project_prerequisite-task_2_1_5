b192e4a5fea83b001782ee652ad28be9265bb21f
Ten Valeriy <walera.tenlist.ru@MacBook-Air-Ten.local>
2025-05-13 07:35:47 +0900

    TEST

--- src/main/java/feast/models/Duck2.java
+++ src/main/java/feast/models/Duck2.java
@@ -1,14 +1,10 @@
 package feast.models;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
 public class Duck2 {
 
     private Chicken3 chicken3;
 
-    @Autowired
+
     public Duck2(Chicken3 chicken3) {
         this.chicken3 = chicken3;
     }
